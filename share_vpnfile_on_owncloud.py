d4e5a7529b71b8b6a9e3b81f8c6a9708bf5809c8
Filipe Castro <44435696+filipeoxs@users.noreply.github.com>
2024-01-18 17:46:05 -0300

    Configurando as ações do selenium

--- share_vpnfile_on_owncloud.py
+++ share_vpnfile_on_owncloud.py
@@ -1,3 +1,4 @@
+# Import necessary modules from Selenium and other libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,7 +6,7 @@ import time
 from selenium.webdriver.chrome.options import Options
 from read_configs import read_configs
 
-# Get URLs from the configuration file
+# Get URLs, username, and password from the configuration file
 cloud_datas = read_configs()
 url_cloud = f'http://{cloud_datas[4]}'
 username = cloud_datas[5]
@@ -16,31 +17,35 @@ chrome_options = Options()
 chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--ignore-ssl-errors')
 
-# Initialize the browser with configured options
+# Initialize the Chrome browser with configured options
 driver = webdriver.Chrome(options=chrome_options)
 
 # Navigate to the first URL
 driver.get(url_cloud)
 
-# Locate the input element by name using By.NAME
+# Locate the input element for username using the name attribute and enter the username
 element_input_user = driver.find_element(By.NAME, 'user').send_keys(username)
 
-# Locate the input element by name using By.NAME
+# Locate the input element for password using the name attribute and enter the password
 element_input_password = driver.find_element(By.NAME, 'password').send_keys(password)
 
+# Click the login button using the element ID
 driver.find_element(By.ID,'submit').click()
 
-# Perform necessary operations...
-# Go to the folder users vpns ipea
+# Navigate to a specific folder in the cloud
 driver.get('https://nuvem.ipea.gov.br/index.php/apps/files/?dir=/VPN%20usu%C3%A1rios%20IPEA&fileid=2606571')
 
-# Create a new folder
+# Create a new folder by clicking on the 'New' button
 driver.find_element(By.CLASS_NAME, 'button new').click()
+
+# Click on the option to create a new folder from the context menu
 driver.find_element(By.XPATH, '//*[@id="body-user"]/div[6]/ul/li[2]/a').click()
-# Inform the name of the folder
+
+# Enter the name of the new folder in the input field
 driver.find_element(By.ID, 'view11-input-folder').send_keys('teste')
 
-
+# Pause execution for 30 seconds
 time.sleep(30)
+
 # Close the browser
 driver.quit()
--- vpn_gen.py
+++ vpn_gen.py
@@ -1,3 +1,4 @@
+# Import necessary modules from Selenium and other libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,27 +6,33 @@ import time
 from selenium.webdriver.chrome.options import Options
 from read_configs import read_configs
 
-# Obtenha as URLs do arquivo de configuração
+# Get URLs from the configuration file
 urls = read_configs()
 urlvpn1 = f'http://{urls[2]}'
+
+# Pause execution for 10 seconds
 time.sleep(10)
+
+# Get the second URL from the configuration file
 urlvpn2 = f'http://{urls[3]}'
 
-# Configurar opções do Chrome para desativar a verificação do certificado SSL
+# Configure Chrome options to disable SSL certificate verification
 chrome_options = Options()
 chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--ignore-ssl-errors')
+
+# Additional capabilities to accept insecure certificates
 capabilities = {
     'acceptInsecureCerts': True
 }
 
-# Inicializar o navegador com as opções configuradas
+# Initialize the Chrome browser with configured options and capabilities
 browser = webdriver.Chrome(options=chrome_options, capabilities=capabilities)
 
-# Navegar para a primeira URL
+# Navigate to the first URL
 browser.get(urlvpn1)
 
-# Realizar as operações necessárias...
+# Perform necessary operations...
 
-# Fechar o navegador
+# Close the browser
 browser.quit()
